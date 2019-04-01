e97ebcad70fb1ff75212caed6af2ad1fd3c28751
Md jabed ali <jabed.akcc@gmail.com>
2019-04-01 13:09:45 +0600

    Update captcha-tst.py

--- captcha-tst.py
+++ captcha-tst.py
@@ -12,12 +12,27 @@ import pytesseract
 import sys
 import os
 import argparse
+import requests
+import time
 from subprocess import check_output
 try:
     import Image
 except ImportError:
     from PIL import Image
 
+apikey = " "
+proxy = "127.0.0.1:0000"
+website = " "
+payload = {
+	'method' : '',
+	'key' : apikey,
+	#'proxy':'127.0.0.1:0000',
+    #'proxytype':"http/https",
+	'body' : '',
+	#'action': 'getbalance'
+}
+
+request = requests.Session()
 pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_tesseract_executable>'
 
 value=Image.open("captcha.jpg")
@@ -29,8 +44,16 @@ print("Captcha text:",text)
 
 #Online captcha solving "https://2captcha.com/" "http://www.imagetyperz.com/"
 #def 2captcha():
-
-
+	resp = requests.get("http://2captcha.com/in.php", params=payload)
+	captchaid = (captchaid.text).split("|")[1]
+	captans = requests.get("http://2captcha.com/res.php?key=" + apikey + "&action=get&id=" + captchaid).text
+	while "CAPCHA_NOT_READY" in captans:
+		captans = requests.get("http://2captcha.com/res.php?key=" + apikey + "&action=get&id=" + captchaid).text
+		time.sleep()
+	captans = captans.split("|")[1]
+	#print(captans)
+	return captans
+		
 def solvecaptcha(path):
 	print("Resizing Image")
 	check_output(['convert', path, '-resize', '600', path])
